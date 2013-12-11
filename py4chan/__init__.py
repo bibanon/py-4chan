6493f81f4332012555d24c26c379ee617a9c4585
Antonizoon <antonizoon@bibanon.org>
2013-12-10 18:22:11 -0800

    fixing the thumbs 4cdn link

--- py4chan/__init__.py
+++ py4chan/__init__.py
@@ -18,7 +18,7 @@ from datetime import datetime
 _4CHAN_BOARDS_URL = 'boards.4chan.org'
 _4CHAN_API = 'a.4cdn.org'
 _4CHAN_IMAGES_URL = 'i.4cdn.org'
-_4CHAN_THUMBS_URL = 't.4cdn.org'
+_4CHAN_THUMBS_URL = '0.t.4cdn.org'
 
 #_4CHAN_BOARDS_URL = 'boards.4chan.org'
 #_4CHAN_API = 'api.4chan.org'
@@ -27,7 +27,7 @@ _4CHAN_THUMBS_URL = 't.4cdn.org'
 
 _BOARD = '%s/%i.json'
 _THREAD = '%s/res/%i.json'
-_VERSION = '0.2.1'
+_VERSION = '0.2.2'
 
 class Board(object):
     def __init__(self, boardName, https = False, apiUrl = _4CHAN_API, session = None):

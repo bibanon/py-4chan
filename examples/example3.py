6ce2201503e6677dbd4c19a42ea261bed3b73817
Antonizoon <antonizoon@bibanon.org>
2014-04-20 16:51:14 -0700

    example code now works on python 2 and 3

--- examples/example3.py
+++ examples/example3.py
@@ -5,12 +5,12 @@ import py4chan
 
 b = py4chan.Board('b')
 threads = b.getThreads()
-print "Got %i threads" % len(threads)
+print("Got %i threads" % len(threads))
 first_thread = threads[0]
-print "First thread: %r" % first_thread
-print "Replies: %r" % first_thread.replies
-print "Updating first thread..."
+print("First thread: %r" % first_thread)
+print("Replies: %r" % first_thread.replies)
+print("Updating first thread...")
 first_thread.update()
-print "First thread now: %r" % first_thread
+print("First thread now: %r" % first_thread)
 for post in first_thread.replies:
-    print post.PostUrl
+    print(post.PostUrl)
